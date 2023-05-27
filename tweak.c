7c8fd6c0973db89b0ba18aa05df1642b331d29bd
Ernest Esene <eroken1@gmail.com>
2023-05-27 12:36:19 +0000

    cleanup request format

--- tweak.c
+++ tweak.c
@@ -14,14 +14,14 @@
 #define MY_METHOD "mymethod: %s\r\n"
 #endif
 
+#define TMP1                                                                   \
+  "%s " PRE_PATH "/proxs/%s%s HTTP/1.1\r\nHost: " PEER_CUSTOM_HOST "\r\n%s"
+#define TMP2 "\r\n" MY_METHOD "\r\n"
+
 /* includes header2 */
-const char *const req_hdr_fmt_worker1 =
-    "%s " PRE_PATH "/proxs/%s%s HTTP/1.1\r\nHost: " PEER_CUSTOM_HOST
-    "\r\n%s\r\n%s\r\n" MY_METHOD "\r\n";
+const char *const req_hdr_fmt_worker1 = TMP1 "\r\n%s" TMP2;
 /* no header2 */
-const char *const req_hdr_fmt_worker2 =
-    "%s " PRE_PATH "/proxs/%s%s HTTP/1.1\r\nHost: " PEER_CUSTOM_HOST
-    "\r\n%s\r\n" MY_METHOD "\r\n";
+const char *const req_hdr_fmt_worker2 = TMP1 TMP2;
 
 /* for custom connect */
 #ifdef PEER_CONNECT_CUSTOM_HOST
